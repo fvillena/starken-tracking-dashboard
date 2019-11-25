ad294906c05caec0565170bcfad71e6b185ede83
Fabián Villena <villenafabian@gmail.com>
2019-11-25 12:57:25 +0100

    links for tracking code and order id

--- js/index.js
+++ js/index.js
@@ -3,10 +3,17 @@ $(document).ready(function() {
         "ajax": "data.php",
         "columns": [
             { "data": "emision" },
-            { "data": "orden" },
+            { "data": "orden",
+            fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html("<a target=_blank href='https://www.starken.cl/seguimiento?codigo="+oData.orden+"'>"+oData.orden+"</a>");
+            }
+        },
             { "data": "destinatario" },
             { "data": "estado" },
-            { "data": "id" }
+            { "data": "id",
+            fnCreatedCell: function (nTd, sData, oData, iRow, iCol) {
+                $(nTd).html("<a target=_blank href='https://dentonet.cl/admin-1470710090/index.php?controller=AdminOrders&vieworder&id_order="+oData.id+"'>"+oData.id+"</a>");
+            } }
         ],
         "paging":false,
         "aaSorting": [0, 'desc'],
